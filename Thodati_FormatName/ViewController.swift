a17e3775c591a847b0d8de0351674d2c47053a62
Saichandhar <tsaichandharrreddy@gmail.com>
2022-02-08 17:13:22 -0600

    Initial Commit

---
+++ Thodati_FormatName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thodati_FormatName
+//
+//  Created by Thodati,Sai Chandhar Reddy on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
