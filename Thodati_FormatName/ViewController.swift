c33d8b716fdba60a695342b3dd306bf511118ec5
Saichandhar <tsaichandharrreddy@gmail.com>
2022-02-08 17:45:38 -0600

    first commit

--- Thodati_FormatName/ViewController.swift
+++ Thodati_FormatName/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBAction func onClickOfSubmit(_ sender: UIButton) {
+        let a=firstNameTextField.text!
+                        
+              let b=lastNameTextField.text!
+                        _=b+a
+                        displayLabel.text="\(b),\(a)"
+    }
+    
+    @IBAction func onClickOfReset(_ sender: UIButton) {
+        firstNameTextField.text = ""
+                lastNameTextField.text = ""
+                displayLabel.text = ""
+                firstNameTextField.becomeFirstResponder()
+    }
+    
+    @IBOutlet weak var displayLabel: UILabel!
 }
 
